9005cfa7069c9e869d1aea1996ff50b2e7af4193
elenayyyy <161946490+elenayyyy@users.noreply.github.com>
2024-12-22 21:00:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 
 # App Title
@@ -7,4 +7,5 @@ st.title('🍪ML Generation')
 
 st.write("""Machine Learning Project""")
 
-
+df = pd.read_csv('https://raw.githubusercontent.com/elenayyyy/dataset/refs/heads/main/food_ingredients_and_allergens.csv?token=GHSAT0AAAAAAC4H5Y3DHWSB26PVOGUL3BHUZ3IBUUQ'),
+df
