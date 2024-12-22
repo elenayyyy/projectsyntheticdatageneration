439e2390bbe97a41186c756104381987828ea792
elenayyyy <161946490+elenayyyy@users.noreply.github.com>
2024-12-22 20:52:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 # App Title
-st.title('☀️ Solar Power Generation')
+st.title('☀️ ML Generation')
 
-st.write("""
-This app uses a real-world dataset to analyze and model solar power generation.  
-The dataset contains information about solar energy generation and weather parameters, enabling predictions of power output.
-""")
+st.write("""Mangine Learning Project""")
 
